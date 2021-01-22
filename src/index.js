7f1911f5efc1f5e7bc5f188a4c6b229d83ea63e1
admin <amanvas2601@gmail.com>
2021-01-22 08:23:06 +0530

    added addperson form and contacts list

--- src/index.js
+++ src/index.js
@@ -1,17 +1,40 @@
-import React from 'react';
+import React , {useState}from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+function AddPersonForm() {
+  const[person, setPerson]=useState("");
+  return(
+    <form>
+      <input type="text"
+      placeholder="add new contact">
+      </input>
+      <button></button>
+    </form>
+  )
+  
+}
+function PeopleList(props) {
+  const arr=props.data;
+  const listItems=arr.map((val,index) =>
+    <li key={index }> {val} </li>
+  );
+
+  return <ol>{listItems}</ol>  
+}
+const contacts=["jethalal","mehta","popatlal"];
+const el=(
+  <div> 
+  
+    <AddPersonForm />
+    <PeopleList data={contacts}/>
+  </div>
+);
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  el,
   document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
